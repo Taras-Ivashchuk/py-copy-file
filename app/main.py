d608a975d61b80323cf7caa903e62b3f4025ca99
Taras Ivashchuk <tvishy@disroot.org>
2025-11-08 23:26:33 +0200

    Variable renamed to have more descriptive names. Updated check for command string arguments

--- app/main.py
+++ app/main.py
@@ -1,19 +1,19 @@
 def copy_file(command: str) -> None:
     tokens = command.split(" ")
-    if len(tokens) < 3:
+    if len(tokens) != 3:
         print("Usage: copy_file(cp f1.txt f2.txt)")
         return
 
-    com, f1, f2 = tokens
+    command, file1, file2 = tokens
 
-    if com != "cp" or f1 == f2:
+    if command != "cp" or file1 == file2:
         print("Usage: copy_file(cp f1.txt f2.txt)")
         return
 
     try:
-        with open(f1, "r") as file_in, open(f2, "w") as file_out:
+        with open(file1, "r") as file_in, open(file2, "w") as file_out:
             for line in file_in.read():
                 file_out.write(line)
 
     except FileNotFoundError:
-        print(f"File {f1} not found")
+        print(f"File {file1} not found")
