6fcd92560397b721f10013a2f3c391239e7d555a
Taras Ivashchuk <tvishy@disroot.org>
2025-11-08 21:33:37 +0200

    Initial commit

--- app/main.py
+++ app/main.py
@@ -1,1 +1,19 @@
-# write your code here
+def copy_file(command: str) -> None:
+    tokens = command.split(" ")
+    if len(tokens) < 3:
+        print("Usage: copy_file(cp f1.txt f2.txt)")
+        return
+
+    com, f1, f2 = tokens
+
+    if com != "cp" or f1 == f2:
+        print("Usage: copy_file(cp f1.txt f2.txt)")
+        return
+
+    try:
+        with open(f1, "r") as file_in, open(f2, "w") as file_out:
+            for line in file_in.read():
+                file_out.write(line)
+
+    except FileNotFoundError:
+        print(f"File {f1} not found")
